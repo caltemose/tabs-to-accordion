2fe5dc243a3b5243bd36b5c5181e6867012ba0a5
Chad Altemose <caltemose@hugeinc.com>
2017-06-06 09:50:15 -0400

    removed unneeded dependencies and cleaned up package file; installed gulp 4

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp')
+
+gulp.task('default', (done) => {
+    done()
+})
+
+
+
+/*
+TODO
+
+Handle markup/templating language (Nunjucks?)
+
+Handle CSS Preprocessing and Autoprefixing
+
+Handle JS ES2015 Transpiling
+(JS Modules/Bundling? Probably not.)
+
+Linters: SASS/CSS, JS
+
+Browsersync
+
+Processing for production (minification, etc)
+
+*/
